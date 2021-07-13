659daf29c7ae0193266064b7bbbaf56320b5a1a1
Aqshola <aqsholafid2@gmail.com>
2021-07-13 20:51:08 +0700

    initialize styleguide

--- src/styles/customTheme.ts
+++ src/styles/customTheme.ts
@@ -1,28 +1,42 @@
 import { theme, extendTheme } from "@chakra-ui/react";
+import "@fontsource/poppins";
 
 const customTheme = extendTheme({
   fonts: {
     ...theme.fonts,
-    body: "Lexend, sans-serif",
-    heading: "Lexend, serif",
+    body: "Poppins, sans-serif",
+    heading: "Poppins, sans-serif",
   },
   colors: {
     ...theme.colors,
-    /** Example */
-    // teal: {
-    //   ...theme.colors.teal,
-    //   700: "#005661",
-    //   500: "#00838e",
-    //   300: "#4fb3be",
-    // },
+    black: "#121212",
+    blackBody: "#313131",
+    pink: "#EA1D76",
+    lightpink: "#F65097",
+    yellow: "#EEE938",
+    blue: "#67CFE3",
+    white: "#FFFFFF",
   },
+
+  fontSizes: {
+    xs: "16px",
+    sm: "18px",
+    md: "20px",
+    lg: "24px",
+    xl: "36px",
+    "2xl": "38px",
+    "3xl": "64px",
+  },
+
   components: {
-    /** Example */
-    // Button: {
-    //   baseStyle: {
-    //     borderRadius: 24,
-    //   },
-    // },
+    Button: {
+      baseStyle: {
+        borderRadius: "6px",
+        fontWeight: "medium",
+        bgColor: "blue",
+        colorScheme: "blue",
+      },
+    },
   },
 });
 
