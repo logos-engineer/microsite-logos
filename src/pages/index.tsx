926be90649961a6276d77dd1c2933fdb2196c156
Sintha Sinurat <sinthadf@gmail.com>
2021-08-11 12:25:57 +0700

    follow us & cta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import Hero from "@/components/homepage/Hero";
 import Layout from "@/components/layout";
+import FollowUs from "@/components/homepage/FollowUs";
+import Cta from "@/components/homepage/Cta";
 
 const Home = () => {
   return (
     <Layout>
       <Hero />
+      <Cta />
+      <FollowUs />
     </Layout>
   );
 };
