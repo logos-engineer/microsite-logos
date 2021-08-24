cc7c196414b1f3f518b9b623019dd64b7908aa6c
Sintha Sinurat <sinthadf@gmail.com>
2021-08-24 12:17:18 +0700

    menambahkan komponen meta-description

--- src/components/layout/Meta.tsx
+++ src/components/layout/Meta.tsx
@@ -1,9 +1,31 @@
 import Head from "next/head";
+import { BRAND, BRAND_TAGLINE, KEYWORDS, SITE } from "constants/texts";
+import { HOME, META_IMAGE } from "constants/paths";
 
-const Meta = () => {
+const Meta = ({
+  title = `${SITE} - ${BRAND}`,
+  description = `${BRAND_TAGLINE}`,
+  url = HOME,
+  image = `${HOME}${META_IMAGE}`,
+  keywords = KEYWORDS,
+}) => {
   return (
     <Head>
-      <title>Logos Microsite</title>
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+      />
+      <title>{title}</title>
+
+      <meta name="description" content={description} key="description" />
+      <meta name="keywords" content={keywords} />
+
+      <link rel="shortcut icon" type="image/ico" href="/favicon.ico" />
+
+      <meta property="og:site_name" content={`${SITE} ${BRAND}`} />
+      <meta key="og-title" property="og:title" content={title} />
+      <meta key="og-image" property="og:image" content={image} />
+      <meta key="og-url" property="og:url" content={url} />
     </Head>
   );
 };
---
+++ src/constants/paths.ts
@@ -0,0 +1,2 @@
+export const HOME: string = `https://microsite-logos.vercel.app`;
+export const META_IMAGE: string = `/img/meta-small.png`;
