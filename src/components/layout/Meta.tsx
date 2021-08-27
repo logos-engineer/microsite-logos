33e8ff52bf026e2607e8e84908965a9395b2cf42
Sintha Sinurat <sinthadf@gmail.com>
2021-08-27 09:15:00 +0700

    menambahkan twitter card pada meta tags

--- src/components/layout/Meta.tsx
+++ src/components/layout/Meta.tsx
@@ -26,6 +26,18 @@ const Meta = ({
       <meta key="og-title" property="og:title" content={title} />
       <meta key="og-image" property="og:image" content={image} />
       <meta key="og-url" property="og:url" content={url} />
+
+      <meta key="tw-image" name="twitter:image" content={image} />
+      <meta name="twitter:image:width" content="1280" />
+      <meta name="twitter:image:height" content="640" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta key="tw-title" name="twitter:title" content={title} />
+      <meta
+        key="tw-description"
+        name="twitter:description"
+        content={description}
+      />
+      <meta key="tw-url" name="twitter:url" content={url} />
     </Head>
   );
 };
